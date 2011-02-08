de96603137625302efa6ff503cc345d331fc78e6
jbotnascimento <jbotnascimento@4a36534f-6be5-5985-a2bf-b84a068b0a8f>
2011-02-08 11:01:52 +0000

    Implementation of some servers method

---
+++ papinho/src/org/server/MainServer.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.server;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author tchchmea
+ */
+public class MainServer {
+
+    public static void main(String... args) {
+
+        try {
+            PapinhoServerIface psi = new PapinhoServer();
+            UnicastRemoteObject.exportObject(psi, 0);
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("server", psi);
+            System.out.println("Server started");
+
+        } catch (Exception e) {
+            System.err.println("Error on server :" + e);
+            return;
+        }
+
+    }
+
+}
