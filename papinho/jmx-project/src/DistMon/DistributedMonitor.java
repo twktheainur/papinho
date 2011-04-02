269ee2af50cb8b3f8835546a626ff21fd95334ef
iegorov.oleg <iegorov.oleg@4a36534f-6be5-5985-a2bf-b84a068b0a8f>
2011-04-02 08:59:39 +0000

    testtest

--- papinho/jmx-project/src/DistMon/DistributedMonitor.java
+++ papinho/jmx-project/src/DistMon/DistributedMonitor.java
@@ -1,12 +1,103 @@
 package DistMon;
 
+import java.util.List;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 public class DistributedMonitor {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-
+	
+	String parent;
+	List<String> children;
+	
+	public DistributedMonitor(String parent, List<String> children) {
+		this.parent = parent;
+		this.children = children;
+		create(parent, true);
+		//for(String i: children)
+		//	create(i, false);
+		
 	}
+	
+	private void create(String destination, boolean ifPublisher) {
+		
+        Context context = null;
+        ConnectionFactory factory = null;
+        Connection connection = null;
+        String factoryName = "ConnectionFactory";
+        String destName = destination;
+        Destination dest = null;
+        int count = 1;
+        Session session = null;
+        MessageProducer sender = null;
+        String text = "Message ";
 
+        try {
+            // create the JNDI initial context.
+            context = new InitialContext();
+
+            // look up the ConnectionFactory
+            factory = (ConnectionFactory) context.lookup(factoryName);
+
+            // look up the Destination
+            dest = (Destination) context.lookup(destName);
+
+            // create the connection
+            connection = factory.createConnection();
+
+            // create the session
+            session = connection.createSession(
+                false, Session.AUTO_ACKNOWLEDGE);
+
+            // create the sender
+            sender = session.createProducer(dest);
+
+            // start the connection, to enable message sends
+            connection.start();
+
+            for (int i = 0; i < count; ++i) {
+                TextMessage message = session.createTextMessage();
+                message.setText(text + (i + 1));
+                sender.send(message);
+                System.out.println("Sent: " + message.getText());
+            }
+        } catch (JMSException exception) {
+            exception.printStackTrace();
+        } catch (NamingException exception) {
+            exception.printStackTrace();
+        } finally {
+            // close the context
+            if (context != null) {
+                try {
+                    context.close();
+                } catch (NamingException exception) {
+                    exception.printStackTrace();
+                }
+            }
+
+            // close the connection
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }
+
+		
+		
+		
+		
+		
+	}
+	
 }
