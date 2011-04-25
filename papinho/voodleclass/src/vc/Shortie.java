59c619085d14e51e2c249919a16d257b8dda6944
twk.theainur <twk.theainur@4a36534f-6be5-5985-a2bf-b84a068b0a8f>
2011-04-25 13:15:10 +0000

    Added a userfeed servlet

--- papinho/voodleclass/src/vc/Shortie.java
+++ papinho/voodleclass/src/vc/Shortie.java
@@ -20,10 +20,9 @@ import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
-
-
 /**
  * Servlet for the user interface, prints out the HTML
+ * 
  * @author jander
  */
 @SuppressWarnings("serial")
@@ -33,7 +32,7 @@ public class Shortie extends HttpServlet {
 		response.setContentType("text/html");
 		String thisURL = request.getRequestURI();
 		UserService userService = UserServiceFactory.getUserService();
-		
+
 		PrintWriter pw = response.getWriter();
 		pw.println("<!DOCTYPE HTML>");
 		pw.println("<html>");
@@ -46,35 +45,42 @@ public class Shortie extends HttpServlet {
 		pw.println("<h1><!--<img src='images/logo.png' alt='Welcome to shortie'/>-->.</h1>");
 		pw.println("<div id='login'>");
 		Principal user = request.getUserPrincipal();
-		if ( user!= null){		
-			String login=String.format("I'm logged in as <strong>%s</strong>(<a href='%s'>sign out</a>)<br/>", request.getUserPrincipal().getName(),userService.createLogoutURL(thisURL));
+		if (user != null) {
+			String login = String
+					.format("I'm logged in as <strong>%s</strong>(<a href='%s'>sign out</a>)<br/>",
+							request.getUserPrincipal().getName(),
+							userService.createLogoutURL(thisURL));
 			pw.println(login);
-		}else {
-	
-			StringBuffer sb=new StringBuffer();
+		} else {
+
+			StringBuffer sb = new StringBuffer();
 			sb.append("I'm an <strong>Anonymous</strong> user(<a href='%s'>sign in</a> with <img src='images/gl.jpg' alt='Google Accounts'/>)<br/>");
-			String htmlparsed=String.format(sb.toString(),userService.createLoginURL(thisURL) );
+			String htmlparsed = String.format(sb.toString(),
+					userService.createLoginURL(thisURL));
 			pw.println(htmlparsed);
-			
+
 		}
 		pw.println("</div>");
 		pw.println("<div id='content'>");
 		pw.println("<div id='post'>");
 		pw.println("<h2>What's on your mind?:</h2>");
 		pw.println("<form action='/post' method='GET'>");
-		String css="";
-		if(request.getAttribute("err")!=null&&request.getAttribute("err").equals("post")){
-			css="border-color:red;";
-		}else{
-			css="";
+		String css = "";
+		if (request.getAttribute("err") != null
+				&& request.getAttribute("err").equals("post")) {
+			css = "border-color:red;";
+		} else {
+			css = "";
 		}
-		
-		if(user==null){
+
+		if (user == null) {
 			pw.println("<span id='anonname'>Post as: <input name='user' type='text' value='Anonymous' onfocus='set_bg(\"#f6F5F3\",this.id)' onblur='set_bg(\"#ffffff\",this.id)' id='uname'/></span>");
 		} else {
 			pw.println("<input name='user' type='hidden' value='Anonymous'/>");
 		}
-		pw.println("<textarea name='post' style='"+css+"'  cols='100' id='textarea' onfocus='set_bg(\"#f6F5F3\",this.id)' onblur='set_bg(\"#ffffff\",this.id)'>");
+		pw.println("<textarea name='post' style='"
+				+ css
+				+ "'  cols='100' id='textarea' onfocus='set_bg(\"#f6F5F3\",this.id)' onblur='set_bg(\"#ffffff\",this.id)'>");
 		pw.println("</textarea>");
 		pw.println("<br/>");
 		pw.println("<input type='submit' name='submit' value='Share it!' id='submit'/>");
@@ -82,30 +88,37 @@ public class Shortie extends HttpServlet {
 		pw.println("</div>");
 		pw.println("<div id='messages'>");
 		pw.println("<h2>My feed:</h2>");
-		
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
 		Query q = new Query("Message");
 		q.addSort("date", SortDirection.DESCENDING);
 
 		PreparedQuery pq = datastore.prepare(q);
 		for (Entity result : pq.asIterable()) {
-			  String name = (String) result.getProperty("userName");
-			  String message = (String) result.getProperty("message");
-			  Date ts = (Date) result.getProperty("date");
-			  
-			  SimpleDateFormat sd=new SimpleDateFormat("dd/MM/yyyy HH:mm");
-			  String tsFormatted=sd.format(ts);
-			  
-			  StringBuffer sb=new StringBuffer();
-			  sb.append("<div class='message'>");
-			  sb.append("<strong>%s</strong> (on %s )<br/>");
-			  sb.append("%s<br/>");
-			  sb.append("</div>");
-			 
-			  String htmlparsed=String.format(sb.toString(),name,tsFormatted,message);
+			String name = (String) result.getProperty("userName");
+			String message = (String) result.getProperty("message");
+			Date ts = (Date) result.getProperty("date");
+			Boolean anon = (Boolean) result.getProperty("anon");
 
-			  pw.println(htmlparsed);
+			SimpleDateFormat sd = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+			String tsFormatted = sd.format(ts);
+
+			StringBuffer sb = new StringBuffer();
+			sb.append("<div class='message'>");
+			if (!anon) {
+				sb.append("<strong><a href='/user/"+user+"'>%s</a></strong> (on %s )<br/>\n");
+			} else {
+				sb.append("<strong>%s</strong> (on %s )<br/>\n");
 			}
+			sb.append("%s<br/>");
+			sb.append("</div>");
+
+			String htmlparsed = String.format(sb.toString(), name, tsFormatted,
+					message);
+
+			pw.println(htmlparsed);
+		}
 		pw.println("</div>");
 		pw.println("</div>");
 		pw.println("</body>");
